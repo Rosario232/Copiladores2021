32c46a7fcc53fb18c421d9a0f5a9b152d5bfd2d6
Rosario232 <89162987+Rosario232@users.noreply.github.com>
2021-08-19 16:57:46 -0500

    Create CaracterSet.h

---
+++ CaracterSet.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#ifndef _CaracterSet_H
+#define _CaracterSet_H
+class CaracterSet
+	{
+private:
+	char *ingresarSimbolos;
+	int numSimbolos;
+public:
+	CaracterSet();
+	CaracterSet(char *caracteres);
+	void setIngresarSimbolos(char *caracteres);
+	char getSimbolo(int i);
+	int getNumSimbolos();
+		
+	};
+
+#endif 
